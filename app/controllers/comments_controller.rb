c1f97df771bb67c41fa922124199e2dba673b380
seiyamiyaoka <seiyamiyaoka@gmail.com>
2017-10-21 15:09:14 +0900

    delete ggg

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,10 +7,11 @@ class CommentsController < ApplicationController
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
         format.js { render :index }
-        unless @comment.blog.user_id == current_user.id
-         Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', { message: 'あなたの作成したブログにコメントが付きました'})
-        end
-          Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', { unread_counts: Notification.where(user_id: @comment.blog.user.id, read: false).count })
+        Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', { message: 'あなたの作成したブログにコメントが付きました'})
+        # unless @comment.blog.user_id == current_user.id
+        #
+        # end
+          # Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', { unread_counts: Notification.where(user_id: @comment.blog.user.id, read: false).count })
       else
         format.html { render :new }
       end
